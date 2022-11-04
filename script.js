8395d7577286dafd65d287c1267e4588bddcb841
ejiro1994 <ejiro1994@gmail.com>
2022-11-04 02:47:49 +0000

    availability animation

--- script.js
+++ script.js
@@ -13,5 +13,16 @@ t1.to('.overlay', {
 })
   .to('.bar1', { x: -4 }, 2)
   .to('.bar2', { x: 4 }, 2)
-  .fromTo('.headers', { y: 30 }, { y: 0, duration: 1 , ease: "power2.inOut"}, 2)
-  .fromTo('.headers', { opacity:0 }, { opacity:1, duration: .7, ease: "power2.inOut" }, 2)
+  .fromTo('.headers', { y: 30 }, { y: 0, duration: 1, ease: 'power2.inOut' }, 2)
+  .fromTo(
+    '.headers',
+    { opacity: 0 },
+    { opacity: 1, duration: 0.7, ease: 'power2.inOut' },
+    2
+  )
+  .fromTo(
+    ['.availContainer', '.goldDot'],
+    { y: 10, opacity: 0 },
+    { y: 0, opacity: 1, duration: 0.5, ease: 'power2.inOut' },
+   3
+  )
