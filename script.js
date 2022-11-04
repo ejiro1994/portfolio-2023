8428c0e7351abc84601ab8a013e6241a5f113d3e
ejiro1994 <ejiro1994@gmail.com>
2022-11-04 13:51:53 +0000

    white light moving and cover moving

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const overlay = document.querySelector('.overlay')
 // const burger = document.querySelector('.burger')
 
+// gsap.registerPlugin(ScrollTrigger)
+
 let t1 = gsap.timeline()
 
 // t1.to('.overlay', { delay: 1.4, duration: 5, top: 1000 })
@@ -11,6 +13,7 @@ t1.to('.overlay', {
   top: 100000,
   ease: Power2.easeInOut,
 })
+  .to('.overlay', {display: 'none', }, 3)
   .to('.bar1', { x: 4 }, 2)
   .to('.bar2', { x: -4 }, 2)
   .fromTo('.headers', { y: 30 }, { y: 0, duration: 1, ease: 'power2.inOut' }, 2)
@@ -32,5 +35,11 @@ t1.to('.overlay', {
   .to('.rotate', { delay: 3.5, transform: 'translateY(-400%)', duration: 0.3 })
   .to('.rotate', { delay: 3.5, transform: 'translateY(-500%)', duration: 0.3 })
   .to('.rotate', { delay: 3.5, transform: 'translateY(-600%)', duration: 0.3 })
-  .to('.cover', { width: 1000 }, 6)
+
+  // should be done on scroll
+  .to('.cover', {   backgroundImage: `radial-gradient(
+    farthest-corner at 70% -100%,
+    #13131300 0%,
+    #131313 56%
+  )` }, 6)
   .to('.color', {x: '75%', opacity: 1, duration:.5},6)
