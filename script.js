d47da307a9c5e077cf568ddba8d84ea2a55fb454
ejiro1994 <ejiro1994@gmail.com>
2022-11-04 02:30:08 +0000

    safari fix for position: relative

--- script.js
+++ script.js
@@ -13,11 +13,5 @@ t1.to('.overlay', {
 })
   .to('.bar1', { x: -4 }, 2)
   .to('.bar2', { x: 4 }, 2)
-  //   .from('.headers', { y: 50 }, 2)
-  //   .from('.headers', { opacity:0, y: 30 })
-  //   .to('.headers', { duration: 1, y: 0 }, 2)
-  //   .to('.headers', { opacity:1, duration: .7 }, 2)
   .fromTo('.headers', { y: 30 }, { y: 0, duration: 1 , ease: "power2.inOut"}, 2)
   .fromTo('.headers', { opacity:0 }, { opacity:1, duration: .7, ease: "power2.inOut" }, 2)
-
-// .to(burger, { duration: 1, top: 1000, ease: Power2.easeInOut }, 2)
