cc08618df99ec742814b74af1d4ad08ee92bcf85
ejiro1994 <ejiro1994@gmail.com>
2022-11-04 12:52:28 +0000

    rotate text using gsap

--- script.js
+++ script.js
@@ -22,7 +22,13 @@ t1.to('.overlay', {
   )
   .fromTo(
     ['.availContainer', '.goldDot'],
-    { y: 10, opacity: 0 }, 
+    { y: 10, opacity: 0 },
     { y: 0, opacity: 1, duration: 0.5, ease: 'power2.inOut' },
-   2.5
+    2.5
   )
+  .to('.rotate', { transform: 'translateY(-100%)', duration: 0.3 }, 5)
+  .to('.rotate', { delay: 3.5, transform: 'translateY(-200%)', duration: 0.3 })
+  .to('.rotate', { delay: 3.5, transform: 'translateY(-300%)', duration: 0.3 })
+  .to('.rotate', { delay: 3.5, transform: 'translateY(-400%)', duration: 0.3 })
+  .to('.rotate', { delay: 3.5, transform: 'translateY(-500%)', duration: 0.3 })
+  .to('.rotate', { delay: 3.5, transform: 'translateY(-600%)', duration: 0.3 })
