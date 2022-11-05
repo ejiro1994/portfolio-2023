35f3f79c06dde5c968d6b68570aab36b36785c5d
ejiro1994 <ejiro1994@gmail.com>
2022-11-05 07:32:51 +0000

    selected works animation

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ t1.to('.overlay', {
   top: 100000,
   ease: Power2.easeInOut,
 })
-  .to('.overlay', {display: 'none', }, 3)
+  .to('.overlay', { display: 'none' }, 3)
   .to('.bar1', { x: 4 }, 2)
   .to('.bar2', { x: -4 }, 2)
   .fromTo('.headers', { y: 30 }, { y: 0, duration: 1, ease: 'power2.inOut' }, 2)
@@ -36,24 +36,37 @@ t1.to('.overlay', {
   .to('.rotate', { delay: 3.5, transform: 'translateY(-400%)', duration: 0.3 })
   .to('.rotate', { delay: 3.5, transform: 'translateY(-500%)', duration: 0.3 })
   .to('.rotate', { delay: 3.5, transform: 'translateY(-600%)', duration: 0.3 })
+  .to(
+    '.lineContainer',
+    {
+      duration: .4,
+      '--translateX': '100%',
+      ease: 'power4.Out',
+    },
+    2.7
+  )
+  .to('.lineContainer', { '--visibility': 'hidden' }, '>.1')
 
-  // should be done on scroll
 
-  const menu = document.querySelector('.nav')
 
- menu.addEventListener('click', (e) => {
-   console.log('hello')
-   t2.to(
-     '.cover',
 
-     {
-       backgroundImage: `radial-gradient(
+// should be done on scroll
+
+const menu = document.querySelector('.nav')
+
+menu.addEventListener('click', (e) => {
+  console.log('hello')
+  t2.to(
+    '.cover',
+
+    {
+      backgroundImage: `radial-gradient(
     farthest-corner at 70% -100%,
     #13131300 0%,
     #131313 56%
   )`,
-       duration: 0.5,
-       ease: 'power3.Out',
-     }
-   )
- })
+      duration: 0.5,
+      ease: 'power3.Out',
+    }
+  )
+})
